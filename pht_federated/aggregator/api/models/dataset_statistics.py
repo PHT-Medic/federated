0fdd5a33cebde41683326d315e249b9b1ddae306
felix6464 <felix.boette@student.uni-tuebingen.de>
2022-09-26 11:06:09 +0200

    resolved most pull request issues

--- pht_federated/aggregator/api/models/dataset_statistics.py
+++ pht_federated/aggregator/api/models/dataset_statistics.py
@@ -1,12 +1,19 @@
 from pht_federated.aggregator.db.base_class import Base
-import uuid
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, JSON
+from sqlmodel import Field
 from datetime import datetime
+import uuid as uuid_pkg
 
 
 class DatasetStatistics(Base):
     __tablename__ = "dataset_statistics"
     #id = Column(String, primary_key=True, default=uuid.uuid4())
+    #id: uuid_pkg.UUID = Field(
+    #    default_factory=uuid_pkg.uuid4,
+    #    primary_key=True,
+    #    index=True,
+    #    nullable=False,
+    #)
     id = Column(Integer, primary_key=True, index=True)
     proposal_id = Column(Integer, ForeignKey('proposals.proposal_id'), index=True)
     # proposal_id = Column(Integer, default=0)
@@ -18,6 +25,12 @@ class DatasetStatistics(Base):
 class Proposals(Base):
     __tablename__ = "proposals"
     #id = Column(String, primary_key=True, default=uuid.uuid4())
+    #id: uuid_pkg.UUID = Field(
+    #    default_factory=uuid_pkg.uuid4,
+    #    primary_key=True,
+    #    index=True,
+    #    nullable=False,
+    #)
     proposal_id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True)
     created_at = Column(DateTime, default=datetime.now())
