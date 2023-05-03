f85226d89da6fe60946b2ad1817bd9dc715a5ce2
felix6464 <felix.boette@student.uni-tuebingen.de>
2023-05-03 08:52:30 +0200

    added pydantic model for difference report

---
+++ pht_federated/aggregator/schemas/difference_report.py
@@ -0,0 +1,37 @@
+import uuid
+from typing import Dict, List, Literal, Optional, Union
+
+from pydantic import BaseModel, Field
+from typing_extensions import Annotated
+
+
+class DifferenceReport(BaseModel):
+    dataset: Optional[str]
+    datatype: Optional[str]
+    status: Optional[str]
+    errors: Optional[
+        List[
+            Annotated[
+                Union[
+                    RowError,
+                    ColumnError,
+                ], Field(discriminator="type"),
+            ]
+        ]
+    ]
+
+
+class RowError(BaseModel):
+    column_name: Optional[str]
+    error_type: Optional[str]
+    suggested_type: Optional[str]
+    row_index: Optional[int]
+    row_value: Optional[str]
+    hint: Optional[str]
+
+
+class ColumnError(BaseModel):
+    column_name: Optional[str]
+    error_type: Optional[str]
+    suggested_name: Optional[str]
+    hint: Optional[str]
